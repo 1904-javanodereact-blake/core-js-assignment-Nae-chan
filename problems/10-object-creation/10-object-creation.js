a13bfbeec7e095fbda21d6b87a3c38c1ec148c3b
nae-chan <danaemorg@gmail.com>
2019-04-03 16:18:50 -0400

    Danae uploaded the solution for problem 10

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,28 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+
+//An object created with object litteral notation
+ let objectOne = {
+     name : 'Blake',
+     age : 25
+ }
+ console.log('ObjectFromObjectLitteral ', objectOne)
+
+ //An object created with constructor function
+ function ObjectFromFunction (name , age) {
+     this.name = name;
+     this.age = age;
+ }
+ let objectTwo = new ObjectFromFunction ('Danae', 24);
+ console.log(objectTwo);
+
+ //An object created with es6 class
+class ObjectFromClass  {
+    constructor (name, age){
+    this.name = name;
+    this.age = age;
+    }
+}
+let objectThree = new ObjectFromClass ('Rick', 21);
+console.log(objectThree);
