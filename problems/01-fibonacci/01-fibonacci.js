5f8c1bc6949b197c7c305db7c250d2ffe47f0c0c
nae-chan <danaemorg@gmail.com>
2019-04-03 00:09:35 -0400

    Danae added the solution to this problem

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -2,5 +2,27 @@
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
 function fib(n) {
-
+    let a = 1;
+    let b = 1;
+    let result = 1;
+    //fibonacci of 1(a) and 2(b) both result in 1
+    if (n < 3){
+        return (result);
+    }else {
+    //use this for loop to track the shifts for fibonacci above 3, repeated nth times
+    console.log(a)
+    console.log(b)
+    for (let count = 1; count < (n-1); count ++) {
+    
+            result = a + b;
+            console.log(result);
+            a = b;
+            b = result;
+        }
+        return (b);
+    }
 }
+console.log(`The nth number is ${fib(8)}`);
+
+
+
