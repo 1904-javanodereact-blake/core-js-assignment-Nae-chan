6e09b2125aafe07ac35d5062433ad8178150b7c3
nae-chan <danaemorg@gmail.com>
2019-04-03 16:40:26 -0400

    Danae uploaded the solution to problem 11

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -3,5 +3,14 @@ Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
 function spliceElement(someArr, index) {
-  
+    someArr.splice(chosenIndex, 0, content);
+  return(someArr);
 }
+let chosenIndex = 1;
+let content = 'insertContentHere';
+let someArr = [1,2,3];
+spliceElement(someArr, chosenIndex);
+
+console.log(someArr, someArr.index);
+
+
