fdb2c19cff59dd683753c0757ecb8870d4ed85cd
nae-chan <danaemorg@gmail.com>
2019-04-03 21:31:01 -0400

    Danae added the solution to problem 9

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -2,5 +2,15 @@
 Define function objectProperties(someObj)
 Print every property and it's value. */
 function objectProperties(someObj) {
-
+//this for in loop iterates through and print the properties of an object
+for (let property in someObj){
+    console.log(`property = ${property} value = ${someObj[property]}`);
+    }
 }
+//Test the above function by passing in this object
+let testObject = {
+    fieldA: 2,
+    fieldB: 20,
+    fieldC: 'apple'
+}
+ objectProperties(testObject);
