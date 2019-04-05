f8169e4b1cbf65ef0efa9283a9a8ea451651673d
nae-chan <danaemorg@gmail.com>
2019-04-05 00:38:04 -0400

    include funtion to throw an exception if input is incorrect

--- problems/05-substring/05-substring.js
+++ problems/05-substring/05-substring.js
@@ -4,10 +4,25 @@ Return the substring contained between startIndex (inclusive) and endIndex (excl
 If incorrect input is entered, throw an exception stating the input was incorrect.
 For example substring('hello', 0, 3) should give 'hel' */
 function substring(someStr, startIndex, endIndex) {
-//turn string into an array and splice at given indice
-let chars = someStr.split('');
-console.log(chars.slice (startIndex, endIndex));
+    //check for correct input
+    try {
+        checkForString(someStr);
+    }
+    catch (e) {
+        console.log(e);
+        return;
+    }
+    //turn string into an array and splice at given indice
+    let chars = someStr.split('');
+    console.log(chars.slice(startIndex, endIndex));
+}
+
+//this function throws an error if variable type is not string
+function checkForString(someStr) {
+    if (typeof someStr !== 'string') {
+        throw "Input is incorrect. Please enter a string!";
+    }
 }
 
 //enter a string, start index, and end index to test the function above
-substring('hello', 0, 3);
+substring(1, 0, 3);
