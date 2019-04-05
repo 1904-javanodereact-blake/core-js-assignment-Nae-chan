47aedd05100d76a7b327bdabae3916c508bbffab
nae-chan <danaemorg@gmail.com>
2019-04-05 00:12:33 -0400

    Danae uploaded the solution to problem 5

--- problems/05-substring/05-substring.js
+++ problems/05-substring/05-substring.js
@@ -4,5 +4,10 @@ Return the substring contained between startIndex (inclusive) and endIndex (excl
 If incorrect input is entered, throw an exception stating the input was incorrect.
 For example substring('hello', 0, 3) should give 'hel' */
 function substring(someStr, startIndex, endIndex) {
-
+//turn string into an array and splice at given indice
+let chars = someStr.split('');
+console.log(chars.slice (startIndex, endIndex));
 }
+
+//enter a string, start index, and end index to test the function above
+substring('hello', 0, 3);
