211911d0eda1930a162dfe8a5187ed8393d4ce87
nae-chan <danaemorg@gmail.com>
2019-04-05 01:55:43 -0400

    Danae added solution to question 2

--- problems/02-bubble-sort/02-bubble-sort.js
+++ problems/02-bubble-sort/02-bubble-sort.js
@@ -3,5 +3,20 @@ Define function: bubbleSort(numArray)
 Use the bubble sort algorithm to sort the array.
 Return the sorted array. */
 function bubbleSort(numArray) {
-  
+    let swapped;
+    do {
+        swapped = false;
+        for (i = 0; i < numArray.length; i++) {
+            if (numArray[i] > numArray[i + 1]) {
+                swapped = true;
+                let temp = numArray[i];
+                numArray[i] = numArray[i + 1];
+                numArray[i + 1] = temp;
+             }
+        }
+    } while (swapped);
+    console.log(numArray);
 }
+
+//pass in an array to test the above functio
+bubbleSort([5, 4, 3, 2, 1]);
