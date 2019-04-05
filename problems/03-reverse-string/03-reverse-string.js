ba3499e3708c74ccee84a75fa51205705a724bcd
nae-chan <danaemorg@gmail.com>
2019-04-05 00:54:12 -0400

    Danae uploaded solution for question 3

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -2,5 +2,12 @@
 Define function: reverseStr(someStr)
 Reverse and return the String. */
 function reverseStr(someStr) {
-  
+    //turn string into an array 
+    let array = someStr.split('');
+    //reverse array and log
+    array.reverse();
+    console.log(array);
 }
+
+//Insert a string to test the function above
+reverseStr("axolotl")
