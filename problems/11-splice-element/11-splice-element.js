fafdb6649856413f56487dd4abce679fe575f771
nae-chan <danaemorg@gmail.com>
2019-04-06 19:03:08 -0400

    Added funtion to remove element with splice

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -2,15 +2,33 @@
 Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
+
+//Not sure if splice to add or splice to delete...
+
+//if splice to add:
 function spliceElement(someArr, index) {
     someArr.splice(chosenIndex, 0, content);
   return(someArr);
 }
+//insert where you would like to splice
 let chosenIndex = 1;
+//insert element to add
 let content = 'insertContentHere';
+//use this array to test the function above
 let someArr = [1,2,3];
 spliceElement(someArr, chosenIndex);
 
-console.log(someArr, someArr.index);
+console.log(someArr);
 
+//if splice to delete:
+function spliceElement2(someOtherArr, otherIndex) {
+  someOtherArr.splice(otherIndex, 1);
+return(someOtherArr);
+}
+//insert where you would like to splice
+let otherChosenIndex = 1;
+//use this array to test the function above
+let someOtherArr = [1,2,3];
+spliceElement2(someOtherArr, otherChosenIndex);
 
+console.log(someOtherArr);
