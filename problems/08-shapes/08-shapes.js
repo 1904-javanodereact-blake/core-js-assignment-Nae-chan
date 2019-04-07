48fcc8586fc56143f6f1fb1750e928b6e17ee35f
nae-chan <danaemorg@gmail.com>
2019-04-07 00:54:31 -0400

    Not complete, just saving this version

--- problems/08-shapes/08-shapes.js
+++ problems/08-shapes/08-shapes.js
@@ -21,50 +21,62 @@ Example for printShape("Diamond", 5, "*");
   * 
 */
 function printShape(shape, height, character) {
-//loop for tracking height
-  for(let h = 1; h < height; h++){
-    //loop for tracking the space before characters
-    for(let s = 1; s < height-1; s++ ){
-      console.log("");
-      //loop for tracking characters
-      for(let c = 1; c <=2; c++) {
-        console.log(character.repeat(c));
-      }
-      
-    }
-}
-console.log(shape);
-}
-function getShape(shape, height, character) {
   //check if height is an odd number
-  if (height %2 == 0) {
-    return console.log ('Please enter an odd number for the height!');
+  if (height % 2 == 0) {
+    return console.log('Please enter an odd number for the height!');
   }
   switch (shape) {
-    case 'square' :
+    case 'square':
       shapeName = 'square';
-      shapeHeight = height
-      shapeCharacter = character
-      printShape(shapeName, shapeHeight, shapeCharacter);
+      console.log('Square');
+      //loop for printing characters
+        for (let c = 1; c < height + 1; c++) {
+          console.log(character.repeat(height));
+        }
       break;
-    case 'triangle' :
+    case 'triangle':
       shapeName = 'Triangle';
-      shapeHeight = height
-      shapeCharacter = character
-      printShape(shapeName, shapeHeight, shapeCharacter);
+      console.log('Triangle');
+      //loop for printing characters
+        for (let c = 1; c < height + 1; c++) {
+          console.log(character.repeat(c));
+        }
       break;
-    case 'diamond' :
-      shapeName = 'Diamond';
-      shapeHeight = height
-      shapeCharacter = character
-      printShape(shapeName, shapeHeight, shapeCharacter);
+    case 'diamond':
+      console.log('Diamond');
+      //loop for tracking the space before characters
+      // for (let s = 1; s < height-1; s++) {
+      //   console.log(`${"".repeat(s)}`)
+      // }
+
+        //loop for tracking characters
+        for (let c = 1; c < height + 1; c++) {
+          let a;
+let b;
+          let space = " ";
+          let countSpace = 5;
+          a = character.repeat(c);
+          b = space.repeat(countSpace);
+          countSpace ++;
+          console.log (`${b}...${a}`);
+          
+        }
+        //loop for tracking characters
+        for (let s = 1; s < height + 1; s++) {
+          let space = "";
+          let countSpace = 1;
+         
+          space ++;
+          
+        }
+        //console.log (`${b}...${a}`);
+      
       break;
   }
-  return shape;
 }
-
-let inputShapeName = 'Square';
+//which shape would you like to print? Specify params to test function above!
+let inputShapeName = 'diamond';
 let inputShapeHeight = 3;
 let inputShapeCharacter = '3';
 
-getShape(inputShapeName.toLowerCase(), inputShapeHeight, inputShapeCharacter);
+printShape(inputShapeName.toLowerCase(), inputShapeHeight, inputShapeCharacter);
